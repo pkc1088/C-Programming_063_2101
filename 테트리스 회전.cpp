eaad64541f4a9b53dc811142d33d1235a0df07b4
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2021-05-13 18:08:55 +0900

    Rename 테트리스 회전2.cpp to 테트리스 회전.cpp

---
+++ 테트리스 회전.cpp
@@ -0,0 +1,19 @@
+#include "Block.h"
+#include "Board.h" 
+
+void Block::rotate_block()
+{
+	int i,j
+	
+	if(Block::can_place_on_board())			// 회전시 보드와 충돌 생기는지 체크		
+	{           
+		Block::erase_block();			// 기존 블록 삭제해 빈공간만들기 
+            	   		 
+        direction = direction + 1;		
+    	if (direction == 4)
+        direction = 0;				// 기존 블록에 회전넣기, 회전4번되면 원래모양으로 
+                          		
+		Block::draw_block();		// 회전된 블록 보드에 나타내기 
+    }
+}
+
